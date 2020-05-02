c762f1c553f3c360778ba5773d52e8ceecbfe5ec
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-02 18:12:03 +0200

    Confirming target directory exists, copying database scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 target=${1:-$PWD/server}
 path_to_react_project=../react
+path_to_database_scripts=../database
 
 echo "Building React node project and copying output files to $target"
 
 mkdir -p $target
+if [ ! -d "$target" ]; then
+    echo "Could not create target directory."
+    exit 1
+fi;
 
 pushd $path_to_react_project > /dev/null
 npm install
@@ -12,7 +17,8 @@ npm run build
 popd > /dev/null
 
 for item in index.html static res; do
-    cp -r $path_to_react_project/build/$item $target
+    /bin/cp -r $path_to_react_project/build/$item $target
 done
+/bin/cp $path_to_database_scripts/*.php $target
 
 echo "Done."
