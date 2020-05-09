faa51ec618ac8a2aa8b8b6735b8933722f5e910e
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-09 16:24:01 +0200

    Consistency: not closing MySQL connection explicitly

--- database/roomname.php
+++ database/roomname.php
@@ -4,7 +4,6 @@ include 'mysql.php';
 
 $result = $sql->query("SELECT propose_name()");
 $name = $result->fetch_row()[0];
-$sql->close();
 
-exit(json_encode(["room_name" => $name]));
+echo json_encode(["room_name" => $name]);
 ?>
