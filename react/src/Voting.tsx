330b5b1ecffaff16eb527ff1c85594cd7ee51031
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-16 11:56:27 +0200

    Refactored WolfVote and VillagerVote into VoteView

--- react/src/Voting.tsx
+++ react/src/Voting.tsx
@@ -6,65 +6,41 @@ import { NameGroup } from './NameGroup'
 
 interface IVoteViewProps {
   role: string;
-  players: string[];
-  werewolf_names: string[];
-  voted_name?: string;
   secret_found: boolean;
   secret: string;
+  werewolf_names: string[];
   seconds_left: number;
-}
-
-export class VoteView extends Component<IVoteViewProps> {
-  render() {
-    let vote;
-    if (!this.props.secret_found)
-      vote = <VillagerVote secret={this.props.secret} players={this.props.players} voted_name={this.props.voted_name}/>;
-    else if (this.props.role === "werewolf")
-       vote = <WolfVote secret={this.props.secret} players={this.props.players} voted_name={this.props.voted_name}/>;
-    else vote = (
-      <div>
-        <div>Das Zauberwort <b>{this.props.secret}</b> wurde erraten!</div>
-        <div><NameGroup items={this.props.werewolf_names} singular="ist der Werwolf" plural="sind die Werwölfe"/>!</div>
-        <div>Werwolf, finde die Seherin!</div>
-      </div>
-    );
-    return <div>{vote}<Countdown seconds_initial={this.props.seconds_left}/></div>
-  }
-}
-
-interface IVotePromptProps {
-  secret: string;
-  players: string[];
-  voted_name: string;
-}
-
-class WolfVote extends Component<IVotePromptProps> {
-  render() {
-    return (
-      <div>
-        <div>Das Zauberwort <b>{this.props.secret}</b>wurde erraten!</div>
-        <div>Finde die Seherin, um die Partie zu gewinnen!</div>
-        <Vote players={this.props.players} voted_name={this.props.voted_name}/>
-      </div>
-    );
-  }
-}
-
-class VillagerVote extends Component<IVotePromptProps> {
-  render() {
-    return (
-      <div>
-        <div>Ihr habt das Zauberwort <b>{this.props.secret}</b> nicht gefunden!</div>
-        <div>Stimmt jetzt darüber ab, wen ihr für den Werwolf haltet!</div>
-        <Vote players={this.props.players} voted_name={this.props.voted_name}/>
-      </div>
-    );
-  }
 }
 
 interface IVoteProps {
   players: string[];
-  voted_name: string;
+  voted_name?: string;
+}
+
+export class VoteView extends Component<IVoteViewProps & IVoteProps> {
+  render() {
+    const isWerewolf = this.props.role === "werewolf";
+    const hasVote = !this.props.secret_found || isWerewolf;
+    const prompt : string = hasVote ?
+      (isWerewolf ?
+        "Finde die Seherin, um die Partie zu gewinnen!" :
+        "Stimmt jetzt darüber ab, wen ihr für den Werwolf haltet!") :
+      "Werwolf, finde die Seherin!";
+    return (
+    <>
+      (this.props.secret_found ?
+        <div>Das Zauberwort <b>{this.props.secret}</b> wurde erraten!</div> :
+        <div>Ihr habt das Zauberwort <b>{this.props.secret}</b> nicht gefunden!</div>
+      )
+      (hasVote ?
+        <Vote players={this.props.players} voted_name={this.props.voted_name}/> :
+        <div><NameGroup items={this.props.werewolf_names} singular="ist der Werwolf" plural="sind die Werwölfe"/>!</div>
+      )
+      <div>{prompt}</div>
+      <div><Countdown seconds_initial={this.props.seconds_left}/></div>
+    </>
+    );
+  }
 }
 
 class Vote extends Component<IVoteProps> {
