c6ec79d4b51a504da259a47810545b13f95f0fed
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-21 19:44:09 +0200

    Add more states to test component

--- react/src/test/TestGameView.tsx
+++ react/src/test/TestGameView.tsx
@@ -19,18 +19,29 @@ export class TestGameView extends React.Component<ISettings, ISettings> {
   }
 
   render() {
+    let role = this.state.role;
+    let is_mayor = this.state.is_mayor;
     let words : string[] | undefined;
-    if (this.state.is_mayor && this.state.state === "choosing") {
-      words = ["geheim", "streng geheim", "unheimlich"];
-    }
-    if (this.state.state === "main" &&
-      (this.state.is_mayor || this.state.role !== "villager")
-    ) {
-      words = ["geheim"];
+    switch (this.state.state) {
+      case "lobby":
+      case "waiting":
+        role = "";
+        is_mayor = false;
+        break;
+      case "choosing":
+        if (this.state.is_mayor)
+          words = ["geheim", "streng geheim", "unheimlich"];
+        break;
+      case "main":
+        if (this.state.is_mayor || role !== "villager")
+          words = ["geheim"];
+        break;
+      case "voting":
+        words = ["geheim"];
     }
 
     let other_werewolves : string[] | undefined;
-    if (this.state.role === "werewolf")
+    if (role === "werewolf")
       other_werewolves = ["Tick", "Trick", "Track"];
 
     const gameView = <GameView
@@ -38,16 +49,16 @@ export class TestGameView extends React.Component<ISettings, ISettings> {
       invite_link=""
       num_players={4}
       other_players={[[1, "X"], [2, "Y"], [3, "Z"]]}
-      is_mayor={this.state.is_mayor}
-      mayor="Heinz"
-      role={this.state.role}
+      is_mayor={is_mayor}
+      mayor="Dieter"
+      role={role}
       difficulty={1}
       num_werewolves={1}
       other_werewolves={other_werewolves}
       words={words}
       />
 
-    const states = ["choosing", "main", "vote"].map((state, index) =>
+    const states = ["lobby", "choosing", "main", "vote", "waiting"].map((state, index) =>
         <label key={index}><input type='radio' checked={state===this.state.state}
                onClick={() => this.setState({state: state})} />{state}</label>);
     const roles = ["villager", "werewolf", "seer"].map((role, index) =>
