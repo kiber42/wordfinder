f577eb3d259a7900e604c1a933aaee004bfe4e1e
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-21 19:28:53 +0200

    Moved SecretRole out of MayorView, added css classes

--- react/src/MayorView.tsx
+++ react/src/MayorView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Connection } from './Context'
-import { SecretRole, ISecretRoleProps } from './SecretRole' // eslint-disable-line no-unused-vars
+import { OtherWerewolves, IOtherWerewolvesProps } from './SecretRole' // eslint-disable-line no-unused-vars
 
 interface IProps {
   role: string;
@@ -9,60 +9,49 @@ interface IProps {
   words?: string[];
 }
 
-export class MayorView extends React.Component<IProps & ISecretRoleProps> {
+export class MayorView extends React.Component<IProps & IOtherWerewolvesProps> {
   static contextType = Connection;
 
-  private getTitle() {
-    switch (this.props.role) {
-      case "werewolf":
-        return "der Bürgermeister-Werwolf";
-      case "seer":
-        return "die Bürgermeister-Seherin";
-      default:
-        return "der Bürgermeister";
-    }
-  }
-
   private getInstructions() {
-    switch (this.props.role) {
-      case "werewolf":
-        return "Mache es den Ratern möglichst schwer, ohne dass sie dir auf die Schliche kommen!";
-      case "seer":
-        return "Mache es den Ratern möglichst einfach, ohne dass der Werwolf dich erkennt!"
-      default:
-        return "Viel Erfolg!";
-    }
+
+    const instructions = {
+      "werewolf": "Mache es den Ratern möglichst schwer, ohne dass sie dir auf die Schliche kommen!",
+      "seer": "Mache es den Ratern möglichst einfach, ohne dass der Werwolf dich erkennt!",
+      "villager": "Viel Erfolg!"
+    };
+    return (
+      <div className="instructions">
+        <div>Die anderen müssen das Wort erraten, aber du darfst nur mit Ja, Nein, und Vielleicht antworten.</div>
+        <div>{instructions[this.props.role]}</div>
+      </div>
+    );
   }
 
   render() {
-    if (this.props.words)
-    {
+    if (this.props.words) {
       const words = this.props.words.map((word, index) => <button onClick={() => this.choose_word(index)} key={index}>{word}</button>);
       return (
-        <div className="player-container mayor-container">
-          <div className="player-info">
-            <div>Du bist der Bürgermeister!</div>
-            <SecretRole role={this.props.role} other_werewolves={this.props.other_werewolves}/>
-            <div>Wähle dein Zauberwort:</div>
-            <div>{words}</div>
-          </div>
+        <div className="player-info">
+          <div className="instructions">Wähle dein Zauberwort:</div>
+          <div>{words}</div>
+          <OtherWerewolves other_werewolves={this.props.other_werewolves}/>
         </div>
       );
     }
 
-    return (
-      <>
-      <div className="player-container mayor-container">
+    if (this.props.secret) {
+      return (
         <div className="player-info">
-          <div>Du bist {this.getTitle()}!</div>
-          <div>Die anderen müssen das Wort erraten, aber du darfst nur mit Ja, Nein, und Vielleicht antworten.</div>
-          <div>Das Zauberwort ist: {this.props.secret}</div>
-          <div>{this.getInstructions()}</div>
+          <div className="secret-message">Das Zauberwort ist: <div className="secret">{this.props.secret}</div></div>
+          {this.getInstructions()}
+          <OtherWerewolves other_werewolves={this.props.other_werewolves}/>
           <button onClick={() => this.secret_found()}>Das Wort wurde erraten!</button>
         </div>
-      </div>
-      </>
-    );
+      );
+    }
+
+    console.error("No words available");
+    return <div>Fehler!</div>
   }
 
   choose_word(index) {
