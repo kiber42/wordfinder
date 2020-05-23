e87c083344a0e579dbb0a9065c0acc037cc98d08
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-23 13:50:41 +0200

    Remove testing code from index.tsx

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import queryString from 'query-string';
 
 import { App } from './App';
-import { TestGameView } from './test/TestGameView';
 
 import './main.scss'
 
@@ -11,11 +10,7 @@ const search = queryString.parse(window.location.search);
 const token = Number(search.token instanceof Array ? search.token[0] : search.token);
 const room_name = search.room_name instanceof Array ? search.room_name[0] : search.room_name;
 
-const testing = true;
-
 ReactDOM.render(
-  testing ?
-    <TestGameView state="choosing" role="werewolf" is_mayor={false} /> :
-    <App token={token} room_name={room_name ?? undefined}/>
+  <App token={token} room_name={room_name ?? undefined}/>
   , document.getElementById('root')
 );
