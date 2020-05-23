3e3754549949a7abdeea1c782050b785776975d8
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-23 11:28:15 +0200

    CSS for settings

--- react/src/Settings.tsx
+++ react/src/Settings.tsx
@@ -2,42 +2,53 @@ import React, { Component } from 'react'
 
 import { Connection } from './Context'
 
-interface DifficultyProps {
-  current: number
+interface IDifficultyProps {
+  current: number;
 }
 
-export class Difficulty extends Component<DifficultyProps> {
+export class Difficulty extends Component<IDifficultyProps> {
   static contextType = Connection;
 
   static levels = ["leicht", "normal", "schwer", "unmöglich"];
 
   render() {
     const choices = Difficulty.levels.map((level, index) =>
-      <label key={index}><input type='radio' checked={index===this.props.current} onClick={() => this.setDifficulty(index)} />{level}</label>);
-    return <div>Schwierigkeit: {choices}</div>
+      <label key={index}><input type="radio" checked={index===this.props.current} onClick={() => this.setDifficulty(index)} />{level}</label>);
+    return (
+      <div className="setting difficulty">
+        <div>Schwierigkeit</div>
+        {choices}
+      </div>
+    );
   }
 
-  setDifficulty(level) {
+  setDifficulty(level: number) {
     fetch("settings.php?token=" + this.context.token + "&difficulty=" + level).then().catch(err => console.error(err));
   }
 }
 
-interface NumWerewolvesProps {
-  current: number,
-  num_players: number
+interface INumWerewolvesProps {
+  current: number;
+  num_players: number;
 }
 
-export class NumWerewolves extends Component<NumWerewolvesProps> {
+export class NumWerewolves extends Component<INumWerewolvesProps> {
   static contextType = Connection;
 
   render() {
     const choices = [1, 2].map(n =>
-      <label key={n}><input type='radio' checked={n===this.props.current} onClick={() => this.setNumber(n)} />{n}</label>);
+      <label key={n}><input type="radio" checked={n===this.props.current} onClick={() => this.setNumber(n)} />{n}</label>);
     const recommended = this.props.num_players <= 6 ? 1 : 2;
-    return <div>Anzahl Werwölfe: {choices} (empfohlen: {recommended})</div>
+    return (
+      <div className="setting numwerewolves">
+        <div>Anzahl Werwölfe</div>
+        {choices}
+        <div>Empfohlen: {recommended}</div>
+      </div>
+    );
   }
 
-  setNumber(n) {
+  setNumber(n: number) {
     fetch("settings.php?token=" + this.context.token + "&num_werewolves=" + n).then().catch(err => console.error(err));
   }
 }
