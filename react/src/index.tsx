1d06afa988ca01c3cc63e5bba7b71246fd16c98d
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-23 22:39:06 +0200

    Preventing NaN token

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -7,10 +7,11 @@ import { App } from './App';
 import './main.scss'
 
 const search = queryString.parse(window.location.search);
-const token = Number(search.token instanceof Array ? search.token[0] : search.token);
-const room_name = search.room instanceof Array ? search.room[0] : search.room;
+const token_str = search.token instanceof Array ? search.token[0] : search.token;
+const token = token_str ? Number(token_str) : undefined;
+const room_name = (search.room instanceof Array ? search.room[0] : search.room) ?? undefined;
 
 ReactDOM.render(
-  <App token={token} room_name={room_name ?? undefined}/>
+  <App token={token} room_name={room_name}/>
   , document.getElementById('root')
 );
