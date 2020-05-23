11f88125ccef127ea38a9f63d4cd9bda7e22bc17
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-23 11:28:41 +0200

    Simplifying team names component

--- react/src/Team.tsx
+++ react/src/Team.tsx
@@ -2,19 +2,15 @@ import React from 'react'
 
 interface ITeamProps {
   active: string[];
-  waiting?: string[];
+  waiting: string[];
 }
 
 export class Team extends React.Component<ITeamProps> {
   public render() {
     const names = this.props.active.join(", ");
-    if (this.props.waiting === undefined)
-    { // in lobby or waiting for game to finish
-      return <div className="teamnames">{this.props.active.length > 0 ? "Mit dir im Raum: " + names : "Du bist alleine im Raum."}</div>;
-    }
     return (
       <>
-        <div className="teamnames">{this.props.active.length > 0 ? "Im Spiel: " + names : "Du bist alleine im Spiel!"}</div>
+        <div className="teamnames">{this.props.active.length > 0 ? "Mitspieler: " + names : "Du bist alleine hier."}</div>
         {this.props.waiting.length > 0 && <div className="waitingnames">In der Lobby: {this.props.waiting.join(", ")}</div>}
       </>
     );
