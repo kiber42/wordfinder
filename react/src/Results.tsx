191a82c3b727914736bcad5030aa2630c5615b17
Kilian Brendel <kilian.brendel@posteo.de>
2020-05-23 12:00:54 +0200

    Styling ResultView

--- react/src/Results.tsx
+++ react/src/Results.tsx
@@ -2,52 +2,53 @@ import React, { Component } from 'react'
 
 import { NameGroup } from './NameGroup'
 
-interface IResultViewProps extends IVoteResultProps {
+interface IResultViewProps {
   secret_found?: number;
   role_found?: number;
   werewolf_names: string[];
   seer_name: string;
+  received_votes?: { [votee: string] : string[] };
 }
 
 export class ResultView extends Component<IResultViewProps> {
-  private getWinnerMessage() {
-    // Secret found and seer found => Werewolf wins
-    // Secret not found and wolf not found => Werewolf wins
-    if (!!this.props.secret_found === !!this.props.role_found) {
-      return <div>Werwolf <NameGroup items={this.props.werewolf_names} singular="hat" plural="haben"/> gewonnen!</div>
-    }
-    return (
-      <div>
-        <div><NameGroup items={this.props.werewolf_names} singular="war der Werwolf" plural="waren die Werwölfe"/>.</div>
-        <div>Die Dorfbewohner haben gewonnen!</div>
-      </div>
-      );
-  }
-
   render() {
     if (this.props.secret_found === undefined || this.props.role_found === undefined)
       return null;
+
+    // Secret found and seer found => Werewolf wins
+    // Secret not found and wolf not found => Werewolf wins
+    const werewolf_won = !!this.props.secret_found === !!this.props.role_found;
+    const werewolf = werewolf_won ?
+      <div>Werwolf <NameGroup items={this.props.werewolf_names} singular="hat" plural="haben"/> gewonnen!</div> :
+      <div><NameGroup items={this.props.werewolf_names} singular="war der Werwolf" plural="waren die Werwölfe"/>.</div>;
+    const seer = <div><b>{this.props.seer_name}</b> war die Seherin.</div>;
+
     return (
-      <div>
-        <div>Die Runde ist zu Ende:</div>
-        <VoteResults received_votes={this.props.received_votes}/>
-        <div><b>{this.props.seer_name}</b> war die Seherin.</div>
-        {this.getWinnerMessage()}
+      <div className="results-container">
+        <div className="results-title">Runde zu Ende</div>
+        <div className="results">
+          {werewolf}
+          {seer}
+          {!werewolf_won && <div>Die Dorfbewohner haben gewonnen!</div>}
+        </div>
+        <VoteResults received_votes={this.props.received_votes ?? {}}/>
       </div>
     );
   }
 }
 
 interface IVoteResultProps {
-  received_votes?: { [votee: string] : string[] };
+  received_votes: { [votee: string] : string[] };
 }
 
 class VoteResults extends Component<IVoteResultProps> {
   render() {
-    if (!this.props.received_votes)
-      return null;
     const votes = this.props.received_votes;
-    const list = Object.keys(votes).map((vote_recipient, index) => {
+    const vote_recipients = Object.keys(votes);
+    if (vote_recipients.length === 0)
+      return <div className="vote-results-container">Niemand hat abgestimmt!</div>
+
+    const list = vote_recipients.map((vote_recipient, index) => {
       const voters = votes[vote_recipient];
       return (
         <li key={index} className="vote-result">
