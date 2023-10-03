6eb0933af2365ab39a514d69a66ac9f4768a7e2e
Kilian Brendel <kilian.brendel@posteo.de>
2023-10-03 08:30:26 +0200

    Update to react 18

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import queryString from 'query-string';
+import React from "react";
+import {createRoot} from "react-dom/client";
+import queryString from "query-string";
 
-import { App } from './App';
+import {App} from "./App";
 
-import './main.scss'
+import "./main.scss";
 
 const search = queryString.parse(window.location.search);
 const token_str = search.token instanceof Array ? search.token[0] : search.token;
 const token = token_str ? Number(token_str) : undefined;
 const room_name = (search.room instanceof Array ? search.room[0] : search.room) ?? undefined;
 
-ReactDOM.render(
-  <App token={token} room_name={room_name}/>
-  , document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = container ? createRoot(container) : null;
+root?.render(<App token={token} room_name={room_name} />);
